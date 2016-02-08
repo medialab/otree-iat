8d0c5f27e3fb9953018200e86fd9cf5b5f77bb05
Davy Peter Braun <hello@davybraun.com>
2016-02-08 18:20:00 +0100

    Working version of IAT in oTree minus styling and displaying X on error.

--- static/js/iat.js
+++ static/js/iat.js
@@ -58,7 +58,22 @@ $(function(window, undefined) {
     var $uiCategoryLeft = $('.left'),
         $uiCategoryRight = $('.right'),
         $uiStimuli = $('.stimuli'),
-        $uiWrongAnswerCross = $('.wrong-answer');
+        $uiWrongAnswerCross = $('.wrong-answer'),
+        $window = $(window);
+
+    var answerStore = {
+      results: [],
+      errors: []
+    };
+
+    /**
+     * Informations to be taken from config.
+     */
+    var keyCodeLeft = null,
+        keyCodeRight = null,
+        answerTimeLimit = null,
+        leftAndRightKeys = {};
+
     /**
      * Update text on UI - left and right category, stimuli word.
      * @param  {Object} data Trial data.
@@ -143,6 +158,7 @@ $(function(window, undefined) {
      * @return {Array}  Array containing two arrays: first order and second order of usable trials.
      */
     function generateExtrasData(name, frameCatStimA, frameCatStimB, extrasCatStim, stimSeqA, stimSeqB) {
+      // `A` mixes in a way the extra is displayed to the left.
       var mixA = {
         name: name,
         categories_stimuli: [
@@ -151,22 +167,23 @@ $(function(window, undefined) {
             stimuli: extrasCatStim.stimuli.concat(frameCatStimA.stimuli)
           },
           {
-            category: frameCatStimB.category + '+' + extrasCatStim.category,
+            category: frameCatStimB.category,
             stimuli: frameCatStimB.stimuli
           }
         ]
       };
 
+      // `A` mixes in a way the extra is displayed to the right.
       var mixB = {
         name: name,
         categories_stimuli: [
           {
-            category: frameCatStimB.category + '+' + extrasCatStim.category,
-            stimuli: extrasCatStim.stimuli.concat(frameCatStimB.stimuli)
+            category: frameCatStimA.category,
+            stimuli: frameCatStimA.stimuli
           },
           {
-            category: frameCatStimA.category + '+' + extrasCatStim.category,
-            stimuli: frameCatStimB.stimuli
+            category: frameCatStimB.category + '+' + extrasCatStim.category,
+            stimuli: extrasCatStim.stimuli.concat(frameCatStimB.stimuli)
           }
         ]
       };
@@ -215,8 +232,15 @@ $(function(window, undefined) {
           trials = trials.concat(extrasData[0], extrasData[1]);
         });
       });
-
       return trials;
+    }
+
+    function setKeyCodesAndTimeLimitFromConfig(dataStore) {
+      keyCodeLeft = dataStore.config.keycodes.left;
+      keyCodeRight = dataStore.config.keycodes.right;
+      answerTimeLimit = dataStore.config.answer_time_limit;
+      leftAndRightKeys[keyCodeLeft] = 'left';
+      leftAndRightKeys[keyCodeRight] = 'right';
     }
 
     /**
@@ -227,9 +251,11 @@ $(function(window, undefined) {
      * @return {Object} A promise resolving with the results payload.
      */
     function loadBlocks(dataStore) {
+      setKeyCodesAndTimeLimitFromConfig(dataStore);
+
       var deferred = $.Deferred();
 
-      startTrial(prepareTrials(dataStore))
+      startBlocks(prepareTrials(dataStore))
         .then(function(results) {
           return deferred.resolve(results);
         });
@@ -237,10 +263,170 @@ $(function(window, undefined) {
       return deferred.promise();
     }
 
-    function startTrial(queue) {
-      var deferred = $.Deferred();
+    /**
+     * Save the correct or wrong input from user, with timing for each round.
+     *
+     * @param  {String} type   'results' or 'errors', according to the keys in `answerStore`.
+     * @param  {Object} trial  The current trial object.
+     * @param  {String} timing The elapsed time for this answer, as taken by the Timer instance.
+     * @return {void}
+     */
+    function save(type, trial, timing, timedOut) {
+      answerStore[type].push(
+        Object.assign({}, trial, {timing: timing, timedOut: timedOut ? true : false}
+      ));
+    }
 
-      deferred.resolve(queue);
+    /**
+     * Promise encapsulating all the process of waiting for the user's answer.
+     * It does resolve anything, but when it does resolve, it means the current trial is finished,
+     * and we have saved results (and optional errors).
+     *
+     * @param  {Object} trial The current trial.
+     * @return {Object} Promise, resolved when trial is done.
+     */
+    function waitForAnswer(trial) {
+      var deferred = $.Deferred();
+      var timer = Timer;
+      var keyPressed = null;
+      var timeLimitForAnswer = null;
+
+      /**
+       * Make everything ready to display the trial for a round.
+       *
+       * @return {void}
+       */
+      function reset() {
+        console.log('showing', trial);
+        dispose();
+
+        timeLimitForAnswer = setTimeout(timeLimitHandler, answerTimeLimit * 1000);
+        $window.on('keyup', keyUpHandler);
+        timer.start();
+      }
+
+      /**
+       * Kill resources used by this round, and reset
+       * the trial so that we are ready to pass it again.
+       *
+       * @return {void}
+       */
+      function dispose() {
+        timer.stop();
+        keyPressed = null;
+
+        clearTimeout(timeLimitForAnswer);
+        $window.off('keyup', keyUpHandler);
+      }
+
+      /**
+       * Handler for user input on keyboard.
+       * Check answer validity and resolve promise if valid.
+       * Otherwise, reset trial.
+       *
+       * @param  {Object} e jQuery.Event passed to handler.
+       * @return {void}
+       */
+      function keyUpHandler(e) {
+        if (!keyPressed) {
+          keyPressed = leftAndRightKeys[e.keyCode];
+
+          if (answerIsOk(keyPressed)) {
+            save('results', trial, timer.getElapsed())
+            dispose();
+            return deferred.resolve();
+          }
+
+          setError(trial, timer.getElapsed());
+        }
+      }
+
+      /**
+       * Save a time out or input error for this trial, then reset.
+       *
+       * @param {Object}  trial    The current trial.
+       * @param {String}  timing   The elapsed time for this answer, as taken by the Timer instance.
+       * @param {Boolean} timedOut Set true to specify the error is a time out.
+       * @return {void}
+       */
+      function setError(trial, timing, timedOut) {
+        save('errors', trial, timing, timedOut);
+        reset();
+      }
+
+      /**
+       * Boolean telling if answer is valid or not.
+       *
+       * @param  {String} keyPressed 'left' or 'right'.
+       * @return {Boolean}
+       */
+      function answerIsOk(keyPressed) {
+        if (keyPressed && trial.correctPosition === keyPressed) {
+          return true;
+        }
+        return false;
+      }
+
+      /**
+       * Triggered on time out... store time out as an error,
+       * and reset the trial.
+       *
+       * @return {void}
+       */
+      function timeLimitHandler() {
+        displayWrongAnswerFeedback(true);
+        setError(trial, timer.getElapsed(), true);
+      }
+
+      // Start process.
+      reset();
+
+      return deferred.promise();
+    }
+
+    /**
+     * Start the queue of blocks of trials.
+     * Built as a promise resolving the answers/errors payload
+     * once the entire suite of blocks is completed.
+     *
+     * @param  {Array} queue Queue of trials objects.
+     * @return {Object} Promise.
+     */
+    function startBlocks(queue) {
+      var deferred = $.Deferred();
+      var currentTrialIndex = 0;
+      var totalNumOfTrials = queue.length;
+
+      /**
+       * Show the specified trial by updating the UI to display its attribute
+       * and returning the promise encapsulating the process of running it.
+       *
+       * @param  {Object} trial The trial to display.
+       * @return {Object} Promise.
+       */
+      var showTrial = function(trial) {
+        updateUIText(trial);
+        return waitForAnswer(trial);
+      };
+
+      var loadTrial = function(trialIndex) {
+        // Pass and show next trial. Do it recursively as long as
+        // you have trials to display. When queue of trials is empty,
+        // resolve promise with all the results.
+        if (trialIndex < totalNumOfTrials) {
+          currentTrialIndex++;
+
+          return showTrial(queue[trialIndex])
+            .then(function() {
+              return loadTrial(currentTrialIndex);
+            });
+        } else {
+          deferred.resolve(answerStore);
+        }
+      };
+
+      // Start first trial.
+      loadTrial(0);
 
       return deferred.promise();
     }
